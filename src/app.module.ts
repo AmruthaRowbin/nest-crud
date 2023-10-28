0c9b2065ea4659264a8a9ee8679ec472d6e7a9c2
ajaykodoth <ajaykodoth1@gmail.com>
2023-10-28 16:13:11 +0530

    commit

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { DatabaseConfig } from './databse/config';
+
+@Module({
+  imports: [TypeOrmModule.forRoot(DatabaseConfig),UserModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/databse/config.ts
@@ -0,0 +1,11 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+export const DatabaseConfig: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: '',
+  database: 'devbyseb',
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  synchronize: true,
+};
